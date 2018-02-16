42f462fe3a589a5f11b6541cb00062baad2e72e6
coco <cocodelacueva@gmail.com>
2018-02-16 07:43:21 -0300

    quite el legal de hit abajo

--- web/footer.php
+++ web/footer.php
@@ -51,12 +51,6 @@
 
     		</div>
     	</section>
-
-        <aside class="hit-legales">
-            <a href="http://hit.com.ar/" target="_blank" class="btn logo-hit-footer">
-                <img src="<?php echo MAINSURL; ?>/assets/images/logo-hit-static.png" alt="Hit" class="logo-hit">
-            </a>
-        </aside>
         
     </footer>
 
